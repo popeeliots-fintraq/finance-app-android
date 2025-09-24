6e7a37dc8914deb273e9419601846d1229670caf
popeeliots-fintraq <pope.eliots@gmail.com>
2025-09-24 20:10:11 +0530

    Create SmsBroadcastReceiver.kt

---
+++ app/src/main/java/com/example/financeapp/SmsBroadcastReceiver.kt
@@ -0,0 +1,21 @@
+package com.example.financeapp
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.provider.Telephony
+import android.util.Log
+
+class SmsBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == Telephony.Sms.Intents.SMS_RECEIVED_ACTION) {
+            val messages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+            for (smsMessage in messages) {
+                val messageBody = smsMessage.messageBody
+                val sender = smsMessage.originatingAddress
+                
+                Log.d("SMS_RECEIVER", "SMS from: $sender, Body: $messageBody")
+            }
+        }
+    }
+}
