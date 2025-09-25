8d002684dcbf80da131e7519a172dfe3076e2c2c
popeeliots-fintraq <pope.eliots@gmail.com>
2025-09-25 21:12:00 +0530

    Create SmsDatabase.kt

---
+++ app/src/main/java/com/example/financeapp/SmsDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.financeapp
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import net.zetetic.database.sqlcipher.SupportFactory
+
+@Database(entities = [SmsData::class], version = 1, exportSchema = false)
+abstract class SmsDatabase : RoomDatabase() {
+
+    abstract fun smsDao(): SmsDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SmsDatabase? = null
+        private val passphrase = SupportFactory("your_secret_passphrase".toByteArray()) 
+
+        fun getDatabase(context: Context): SmsDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    SmsDatabase::class.java,
+                    "sms_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .openHelperFactory(passphrase)
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
