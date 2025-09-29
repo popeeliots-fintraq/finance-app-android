afc0650d85eaa5a55a33dddfc46629d27d96479c
popeeliots-fintraq <pope.eliots@gmail.com>
2025-09-29 15:47:34 +0530

    Update SmsDatabase.kt

--- app/src/main/java/com/example/financeapp/SmsDatabase.kt
+++ app/src/main/java/com/example/financeapp/SmsDatabase.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import net.zetetic.database.sqlcipher.SupportFactory
+import net.sqlcipher.database.SupportFactory
 
 @Database(entities = [SmsData::class], version = 1, exportSchema = false)
 abstract class SmsDatabase : RoomDatabase() {
