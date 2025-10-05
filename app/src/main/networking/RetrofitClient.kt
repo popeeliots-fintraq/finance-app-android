d28a6a6e27d3dc41c5ef0d3c802e24be05135418
popeeliots-fintraq <pope.eliots@gmail.com>
2025-10-05 17:13:58 +0530

    Create RetrofitClient.kt

---
+++ app/src/main/networking/RetrofitClient.kt
@@ -0,0 +1,26 @@
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    private const val BASE_URL = "https://categorizetransaction-e6n3cyilhq-uc.a.run.app/"
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val api: CategorizeApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CategorizeApi::class.java)
+    }
+}
