0b67c763767a0a452cad20184f6727d5b30e4336
popeeliots-fintraq <pope.eliots@gmail.com>
2025-10-10 01:37:50 +0530

    Update CategorizerService.kt

--- app/src/main/java/com/example/financeapp/data/remote/CategorizerService.kt
+++ app/src/main/java/com/example/financeapp/data/remote/CategorizerService.kt
@@ -3,9 +3,13 @@ package com.example.financeapp.data.remote
 import com.example.financeapp.data.model.CategorizationResponse
 import com.example.financeapp.data.model.TransactionRequest
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface CategorizerService {
+
+    @GET("/")
+    suspend fun healthCheck(): String
 
     @POST("/categorize")
     suspend fun categorizeTransaction(
