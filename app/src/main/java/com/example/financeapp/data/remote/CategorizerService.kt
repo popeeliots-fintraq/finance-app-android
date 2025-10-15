35fa8e794ddaf57290331548686b6a104cf2ff10
popeeliots-fintraq <pope.eliots@gmail.com>
2025-10-15 17:58:43 +0530

    Update CategorizerService.kt

--- app/src/main/java/com/example/financeapp/data/remote/CategorizerService.kt
+++ app/src/main/java/com/example/financeapp/data/remote/CategorizerService.kt
@@ -4,7 +4,7 @@ import com.example.financeapp.data.model.CategorizationResponse
 import com.example.financeapp.data.model.TransactionRequest
 import retrofit2.Response // <-- Import the Response class
 import retrofit2.http.Body
-import retrofit22.http.GET
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface CategorizerService {
