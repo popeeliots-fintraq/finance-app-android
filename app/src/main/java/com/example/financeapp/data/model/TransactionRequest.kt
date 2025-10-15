3164e66718b8ffa2760770f4b8a33d52c6f4b332
popeeliots-fintraq <pope.eliots@gmail.com>
2025-10-15 16:29:46 +0530

    Update TransactionRequest.kt

--- app/src/main/java/com/example/financeapp/data/model/TransactionRequest.kt
+++ app/src/main/java/com/example/financeapp/data/model/TransactionRequest.kt
@@ -1,5 +1,9 @@
 package com.example.financeapp.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class TransactionRequest(
+    // Backend expects 'sms_text', but we want to call it 'description' in Kotlin
+    @SerializedName("sms_text") 
     val description: String
 )
