798e8f48f822ed6f9eb026712cabf69eb1888dd4
popeeliots-fintraq <pope.eliots@gmail.com>
2025-10-17 02:26:01 +0530

    Update SmsDatabase.kt

--- app/src/main/java/com/example/financeapp/SmsDatabase.kt
+++ app/src/main/java/com/example/financeapp/SmsDatabase.kt
@@ -11,7 +11,7 @@ import com.example.financeapp.data.local.LeakBucket
 import com.example.financeapp.data.local.SalaryBucketDao
 import com.example.financeapp.data.local.LeakBucketDao
 
-@Database(entities = [SmsData::class, SalaryBucket::class, LeakBucket::class], version = 2, exportSchema = false)
+@Database(entities = [SmsData::class, SalaryBucket::class, LeakBucket::class], version = 3, exportSchema = false)
 abstract class SmsDatabase : RoomDatabase() {
 
     abstract fun smsDao(): SmsDao
