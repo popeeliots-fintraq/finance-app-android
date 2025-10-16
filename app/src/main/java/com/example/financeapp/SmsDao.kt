916109b70d727a0dba3ec39414692c5c56a1c054
popeeliots-fintraq <pope.eliots@gmail.com>
2025-10-17 00:39:31 +0530

    Update SmsDao.kt

--- app/src/main/java/com/example/financeapp/SmsDao.kt
+++ app/src/main/java/com/example/financeapp/SmsDao.kt
@@ -3,9 +3,12 @@ package com.example.financeapp
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Update
 
 @Dao
 interface SmsDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(smsData: SmsData)
+    @Update
+    suspend fun update(smsData: SmsData)
 }
