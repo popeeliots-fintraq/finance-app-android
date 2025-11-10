c580ec9957363a16e6cca5eac11562293f6703e3
popeeliots-fintraq <pope.eliots@gmail.com>
2025-11-10 21:37:37 +0530

    Create LeakageOut.kt

---
+++ app/src/main/java/com/example/financeapp/data/model/LeakageOut.kt
@@ -0,0 +1,20 @@
+// LeakageOut.kt
+package com.example.financeapp.data.model // This is your correct package path
+
+import kotlinx.serialization.Serializable
+import java.math.BigDecimal
+import java.time.LocalDate 
+
+@Serializable
+data class LeakageOut(
+    val total_reclaimable_salary: BigDecimal, 
+    val if_leak_fixed_new_salary: BigDecimal, 
+    val net_monthly_income: BigDecimal,
+    val reporting_period: LocalDate,
+    
+    // References LeakageBucketNetwork.kt
+    val leakage_buckets: List<LeakageBucketNetwork>, 
+
+    val e_family_size: BigDecimal,
+    val essential_target: BigDecimal
+)
