35ef2250de4d602b4a8d5d6f90fe8e0e68722a19
popeeliots-fintraq <pope.eliots@gmail.com>
2025-11-26 17:52:15 +0530

    Create SmsWorkerConstants.kt

---
+++ app/src/main/java/com/example/financeapp/workers/SmsWorkerConstants.kt
@@ -0,0 +1,6 @@
+package com.example.financeapp.workers
+
+// Centralized constants for passing data between the BroadcastReceiver and WorkManager
+const val KEY_RAW_SMS_TEXT = "raw_sms_text"
+const val KEY_SMS_TIMESTAMP = "sms_timestamp"
+const val KEY_USER_ID = "user_id"
