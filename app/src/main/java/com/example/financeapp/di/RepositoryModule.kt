6456b383b5a807936d29d3ba2c257046b4bff333
popeeliots-fintraq <pope.eliots@gmail.com>
2025-11-30 17:41:40 +0530

    Update RepositoryModule.kt

--- app/src/main/java/com/example/financeapp/di/RepositoryModule.kt
+++ app/src/main/java/com/example/financeapp/di/RepositoryModule.kt
@@ -1,14 +1,3 @@
-package com.example.financeapp.di
-
-import com.example.financeapp.api.ApiService
-import com.example.financeapp.data.dao.*
-import com.example.financeapp.data.repository.FinanceRepository
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
-
 @Module
 @InstallIn(SingletonComponent::class)
 object RepositoryModule {
@@ -17,17 +6,12 @@ object RepositoryModule {
     @Singleton
     fun provideFinanceRepository(
         apiService: ApiService,
-        smsDao: SmsDao,
-        transactionDao: TransactionDao,
-        salaryBucketDao: SalaryBucketDao,
-        leakBucketDao: LeakBucketDao
+        smsDao: SmsDao
     ): FinanceRepository {
         return FinanceRepository(
-            apiService,
-            smsDao,
-            transactionDao,
-            salaryBucketDao,
-            leakBucketDao
+            apiService = apiService,
+            smsDao = smsDao
+            // remove transactionDao, categoryDao, budgetDao until they exist
         )
     }
 }
