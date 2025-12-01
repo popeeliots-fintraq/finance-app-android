4957e32459e9c7748700fd2200131e730d47fa44
popeeliots-fintraq <pope.eliots@gmail.com>
2025-12-01 18:01:47 +0530

    Create ApiModule.kt

---
+++ app/src/main/java/com/example/financeapp/di/ApiModule.kt
@@ -0,0 +1,32 @@
+package com.example.financeapp.di
+
+import com.example.financeapp.data.api.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+
+    private const val BASE_URL = "https://yourapi.com/" // Replace with actual base URL
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+}
