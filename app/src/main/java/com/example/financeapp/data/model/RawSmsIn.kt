4942eabba2dcfee2e9346cf0deaa567ef1858e41
popeeliots-fintraq <pope.eliots@gmail.com>
2025-12-04 15:24:47 +0530

    Update RawSmsIn.kt

--- app/src/main/java/com/example/financeapp/data/model/RawSmsIn.kt
+++ app/src/main/java/com/example/financeapp/data/model/RawSmsIn.kt
@@ -3,10 +3,6 @@ package com.example.financeapp.data.model
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
-// Note: Removed the old Kotlin Serialization import: 
-// import kotlinx.serialization.Serializable
-// import kotlinx.serialization.SerialName
-
 @JsonClass(generateAdapter = true)
 data class RawSmsIn(
     @Json(name = "sms_text")
@@ -16,11 +12,8 @@ data class RawSmsIn(
     val senderId: String,
 
     @Json(name = "timestamp")
-    val timestamp: String, // Use a proper date/time type if possible, e.g., Instant
+    val timestamp: String, // Convert Long -> ISO string in worker
 
     @Json(name = "user_id")
     val userId: String
 )
-// If you need it to be Serializable for Parceling/passing between components, 
-// you would implement the standard Java/Kotlin Serializable interface:
-// : java.io.Serializable
